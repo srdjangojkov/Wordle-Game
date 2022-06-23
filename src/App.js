32b1b78804fc774dd3c655b2c3cddb73bcf992ce
Srđan Gojkov <srdjaliman@gmail.com>
2022-06-23 15:50:30 +0200

    Create staring component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Wordle from "./components/Wordle";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [solution, setSolution] = useState(null);
+
+	useEffect(() => {
+		fetch("http://localhost:3001/solutions")
+			.then((resp) => resp.json())
+			.then((json) => {
+				const randomSolution = json[Math.floor(Math.random() * json.length)];
+				setSolution(randomSolution.word);
+			});
+	}, []);
+
+	return (
+		<div className="App">
+			<h1>Wordle</h1>
+			{solution && <Wordle solution={solution} />}
+		</div>
+	);
 }
 
 export default App;
